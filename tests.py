a3018880b448bf372b986891786a8a99310d4032
Svajunas Koncius <svajunas.koncius@mera.com>
2024-12-02 14:59:19 +0200

    Included messages in tests

--- tests.py
+++ tests.py
@@ -5,25 +5,25 @@ from decorator import helloworld
 
 
 def test_fibonnaci():
-    assert fibonacci(5) == 5
-    assert fibonacci(6) == 8
-    assert fibonacci(-1) == -1
-    assert fibonacci(10) == 55
+    assert fibonacci(5) == 5, "Provided number isn't == 5"
+    assert fibonacci(6) == 8, "Provided number isn't == 8"
+    assert fibonacci(-1) == -1, "Provided number isn't == -1"
+    assert fibonacci(10) == 55, "Provided number isn't == 55"
 
 
 def test_lru_fibonnaci():
-    assert lru_fibonacci(5) == 5
-    assert lru_fibonacci(6) == 8
-    assert lru_fibonacci(-1) == -1
-    assert lru_fibonacci(10) == 55
+    assert lru_fibonacci(5) == 5, "Provided number isn't == 5"
+    assert lru_fibonacci(6) == 8, "Provided number isn't == 8"
+    assert lru_fibonacci(-1) == -1, "Provided number isn't == -1"
+    assert lru_fibonacci(10) == 55, "Provided number isn't == 55"
 
 
 def test_Fibo():
-    fibo = Fibo()
-    assert fibo.fibonacci(5) == 5
-    assert fibo.fibonacci(6) == 8
-    assert fibo.fibonacci(-1) == -1
-    assert fibo.fibonacci(10) == 55
+    fibo = Fibo(6)
+    assert fibo.get_fibonnaci_number() == 5, "Provided number isn't == 5"
+    assert fibo.get_fibonnaci_number() == 8, "Provided number isn't == 8"
+    assert fibo.get_fibonnaci_number() == -1, "Provided number isn't == -1"
+    assert fibo.get_fibonnaci_number() == 55, "Provided number isn't == 55"
 
 
 def test_helloWorld():
