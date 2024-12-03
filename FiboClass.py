16db2c0c25a9d64e854438b5a2aba0d3c96dc1ef
Svajunas Koncius <svajunas.koncius@mera.com>
2024-12-03 10:45:08 +0200

    Modify Fibo class with autopep8

--- FiboClass.py
+++ FiboClass.py
@@ -10,10 +10,11 @@ class Fibo:
         if number <= 1:
             return number
         else:
-            return self.fibonacci(number-2) + self.fibonacci(number-1)
+            return self.fibonacci(number - 2) + self.fibonacci(number - 1)
 
     def get_fibonnaci_number(self):
         return self.fibonacci(self.number)
+
 
 fibo = Fibo(6)
 
