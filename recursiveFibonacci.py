7058eba46b4bbe46d4c4cf844310fb11038050d5
Svajunas Koncius <svajunas.koncius@mera.com>
2024-12-03 12:05:36 +0200

    Create fibonacci with my own cache

--- recursiveFibonacci.py
+++ recursiveFibonacci.py
@@ -1,6 +1,18 @@
 from functools import lru_cache
+from time import perf_counter
 
 
+def count_time(func):
+    def inner(n):
+        start = perf_counter()
+        func(n)
+        end = perf_counter()
+        performance_time = end - start
+        return performance_time
+    return inner
+
+
+@count_time
 def fibonacci(number):
     if number <= 1:
         return number
@@ -14,5 +26,13 @@ def lru_fibonacci(number):
     return fibonacci(number - 2) + fibonacci(number - 1)
 
 
-print(fibonacci(5))
-print(lru_fibonacci(5))
+def cache_fibbonacci(number, cache=None):
+    if cache is None:
+        cache = {}
+    if number in cache:
+        return cache[number]
+    if number <= 1:
+        return number
+    result = cache_fibbonacci(number - 2) + cache_fibbonacci(number - 1)
+    cache[number] = result
+    return result
